860a55edb1c152d4c0ced1a1a1573ea80df275ad
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-23 22:42:38 -0400

    upgrading to meteor 0.9.0

--- package.js
+++ package.js
@@ -1,10 +1,17 @@
 Package.describe({
-  summary: "jQuery barcode plugin - a jQuery plugin for generating barcodes"
+  summary: "jQuery barcode plugin - a jQuery plugin for generating barcodes",
+  // Version number.
+  version: "2.0.3",
+  // Optional.  Default is package directory name.
+  name: "steeve:jquery-barcode",
+  // Optional github URL to your source repository.
+  git: "https://github.com/EtherPOS/jquery-barcode.git",
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
   api.use('jquery', 'client');
-  api.add_files([
+  api.addFiles([
     'lib/jquery-barcode-2.0.2.min.js',
   ], 'client'
   );
